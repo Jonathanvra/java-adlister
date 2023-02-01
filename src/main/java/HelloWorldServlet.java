0e2593c4e14c3991d00920fc8a0416a030d91847
Jonathan Vargas <jonatan.vargas3339@gmail.com>
2023-02-01 16:16:03 -0600

    finished servlet-exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,27 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        int count;
+        try {count = Integer.parseInt(req.getParameter("count"));
+        } catch (Exception e) {
+            count = 1;
+        }
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+//   Add name
+        String name = req.getParameter("name");
+        if (name == null) {
+            name = "World";
+        }
+        for (int i = 0; i < count; i++)
+            out.println("<h1>Hello, " + name + "!</h1>");
     }
 }
