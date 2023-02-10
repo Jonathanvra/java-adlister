4ab6a597b3562731f444c0f5a71ba10f666b53a5
Jonathan Vargas <jonatan.vargas3339@gmail.com>
2023-02-10 12:32:10 -0600

    pizza order exercise

--- src/main/java/PizzaOrderServlet.java
+++ src/main/java/PizzaOrderServlet.java
@@ -1,7 +1,14 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(name = "PizzaOrderServlet", urlPatterns = "/pizza-order")
 public class PizzaOrderServlet extends HttpServlet {
-
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        req.getRequestDispatcher("pizza-form.jsp").forward(req, res);
+    }
 }
